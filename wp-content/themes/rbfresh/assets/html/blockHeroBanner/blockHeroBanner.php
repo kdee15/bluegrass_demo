90270e1a697d2a6605c60c5beb27aa3d30e7ee72
intembeko <kirkdaniels@me.com>
2025-05-21 16:11:27 +0200

    setup

--- wp-content/themes/rbfresh/assets/html/blockHeroBanner/blockHeroBanner.php
+++ wp-content/themes/rbfresh/assets/html/blockHeroBanner/blockHeroBanner.php
@@ -9,10 +9,13 @@
 <section class="hero">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-12 hero__content">
-            <div id="hero-carousel-app"></div>
+            <div class="col-12 col-md-6 hero__content">
+                <h1 class="hero__title">test hi</h1>
+                <!-- Add more content here -->
             </div>
-  
+            <div class="col-12 col-md-6 hero__image">
+                <!-- Add hero image here -->
+            </div>
         </div>
     </div>
 </section>
--- wp-content/themes/rbfresh/functions.php
+++ wp-content/themes/rbfresh/functions.php
@@ -5,13 +5,9 @@
 
 // Theme setup
 function rbfresh_setup() {
-    // Add default posts and comments RSS feed links to head
+    // Add theme support
     add_theme_support('automatic-feed-links');
-
-    // Let WordPress manage the document title
     add_theme_support('title-tag');
-
-    // Enable support for Post Thumbnails on posts and pages
     add_theme_support('post-thumbnails');
 
     // Register navigation menus
@@ -33,6 +29,22 @@ add_action('after_setup_theme', 'rbfresh_setup');
 
 // Enqueue scripts and styles
 function rbfresh_scripts() {
+    // Deregister default WordPress jQuery and enqueue a newer version
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.7.1.min.js', array(), '3.7.1', true);
+
+    // Enqueue Bootstrap CSS
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all');
+
+    // Enqueue Bootstrap JS
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true);
+
+    // Enqueue Swiper CSS
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.0', 'all');
+
+    // Enqueue Swiper JS
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.0', true);
+
     // Enqueue main stylesheet (style.css in theme root)
     wp_enqueue_style('rbfresh-style', get_stylesheet_uri());
     
@@ -56,6 +68,78 @@ add_action('wp_enqueue_scripts', 'rbfresh_scripts');
  * Include the dynamic CSS file
  */
 require get_template_directory() . '/inc/dynamic-css.php';
+
+
+
+
+// A.2 CUSTOM CONTENT TYPES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// A.2.1. HOMEPAGE CAROUSEL ---------------------------------------------------------------------------------------
+
+function carousel() {
+    $labels = array(
+      'Title'              => _x( 'Hero Carousel', 'post type general name' ),
+      'singular_name'      => _x( 'Carousel Slides', 'post type singular name' ),
+      'add_new'            => _x( 'Add New', 'Carousel Slide' ),
+      'add_new_item'       => __( 'Add New Carousel Slide' ),
+      'edit_item'          => __( 'Edit Carousel Slide' ),
+      'new_item'           => __( 'New Carousel Slide' ),
+      'all_items'          => __( 'All Carousel Slides' ),
+      'view_item'          => __( 'View Carousel Slide' ),
+      'parent_item_colon'  => '',
+      'menu_name'          => 'Hero Carousel'
+    );
+  
+    $args = array(
+      'labels'         => $labels,
+      'description'   => 'A list of Carousel Slides',
+      'public'        => true,
+      'menu_position' => 3,
+      'supports'      => array( 'title', 'editor', 'thumbnail', 'taxonomies', 'categories', 'media', 'content' ),
+      'has_archive'   => true,
+  
+    );
+  
+    register_post_type( 'carousel', $args );
+  }
+  
+  add_action( 'init', 'carousel' );
+  
+// A.2.1. End -----------------------------------------------------------------------------------------------------
+
+// A.2.2. SIX CARD GRID -------------------------------------------------------------------------------------------
+
+function card_grid() {
+    $labels = array(
+      'Title'              => _x( 'Six Card Grid', 'post type general name' ),
+      'singular_name'      => _x( 'Card', 'post type singular name' ),
+      'add_new'            => _x( 'Add New', 'Card' ),
+      'add_new_item'       => __( 'Add New Card' ),
+      'edit_item'          => __( 'Edit Card' ),
+      'new_item'           => __( 'New Card' ),
+      'all_items'          => __( 'All Cards' ),
+      'view_item'          => __( 'View Card' ),
+      'parent_item_colon'  => '',
+      'menu_name'          => 'Six Card Grid'
+    );
+  
+    $args = array(
+      'labels'         => $labels,
+      'description'   => 'A list of Cards',
+      'public'        => true,
+      'menu_position' => 4,
+      'supports'      => array( 'title', 'editor', 'thumbnail', 'taxonomies', 'categories', 'media', 'content' ),
+      'has_archive'   => true,
+  
+    );
+  
+    register_post_type( 'card_grid', $args );
+  }
+  
+  add_action( 'init', 'card_grid' );
+  
+// A.2.2. End -----------------------------------------------------------------------------------------------------
+
 
 // Register widget area (sidebar)
 function rbfresh_widgets_init() {
