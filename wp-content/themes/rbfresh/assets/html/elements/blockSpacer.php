6adbe361303e763c0e5e2a3e4679bada62e69336
intembeko <kirkdaniels@me.com>
2025-05-23 10:20:47 +0200

    block spacer

---
+++ wp-content/themes/rbfresh/assets/html/elements/blockSpacer.php
@@ -0,0 +1,8 @@
+<div class="block-spacer">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 50" class="spacer__left" preserveAspectRatio="none">
+        <polygon points="100 50 0 50 0 0 100 50" fill="yellow"/>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 50" class="spacer__right" preserveAspectRatio="none">
+        <polygon points="0 50 100 50 100 0 0 50" fill="green"/>
+    </svg>
+</div>
--- wp-content/themes/rbfresh/front-page.php
+++ wp-content/themes/rbfresh/front-page.php
@@ -10,7 +10,9 @@ get_header(); ?>
 <main id="primary" class="site-main">
 	<?php get_template_part( 'assets/html/blocks/blockHeroBanner' ); ?>
 	<?php get_template_part( 'assets/html/blocks/blockCardGrid' ); ?>
+	<?php get_template_part( 'assets/html/elements/blockSpacer' ); ?>
 	<?php get_template_part( 'assets/html/blocks/blockTwoColumn' ); ?>
+	<?php get_template_part( 'assets/html/elements/blockSpacer' ); ?>
 	<?php get_template_part( 'assets/html/blocks/blockTwoColumn' ); ?>
 </main>
 
